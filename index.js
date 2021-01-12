806790a7d65f7b4a296c3884adbb8e8379496c3b
KSFlynn007 <kelseyflynnn@gmail.com>
2021-01-12 12:05:42 -0700

    Update index.js
    finalized endpoint logic

--- index.js
+++ index.js
@@ -58,10 +58,10 @@ app.get('/movies/:Title', (req, res) => {
 // should it be as is or just 
 app.get('/movies/genres/:Name', (req, res) => {
 // This is how it is searched by - by name of GENRE, not name of MOVIE
-    Movies.findOne({ 'Name': req.params.Name })
-// This is what is returned in JSON format, I only need Genre Name + Genre Description, not entire movie
+    Movies.findOne({ 'Genre.Name': req.params.Name })
+// This is what is returned in JSON format, I only need Genre Name + Genre Description, not entire movie details
     .then((movie) => {
-        res.json(movie);
+        res.status(201).json(movie.Genre.Name + ": " + movie.Genre.Description);
     })
     .catch((err) => {
         console.error(err);
@@ -71,9 +71,9 @@ app.get('/movies/genres/:Name', (req, res) => {
 
 //get data about a director, same format as above by searching in nested content?
 app.get('/movies/directors/:Name', (req, res) => {
-    Movies.findOne({ Director: req.params.Director.Name })
+    Movies.findOne({ 'Director.Name': req.params.Name })
     .then((movie) => {
-        res.json(movie);
+        res.status(201).json(movie.Director.Name + ': ' + movie.Director.Bio);
     })
     .catch((err) => {
         console.error(err);
